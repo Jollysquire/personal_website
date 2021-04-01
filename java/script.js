b559b50c39b7451722be7ab6db7af8a76816fd63
Jolly <jmurphy106@my.bcit.ca>
2021-04-01 04:20:23 -0700

    added a do/while loop to check if user input is a number that is 4 digits long

--- java/script.js
+++ java/script.js
@@ -3,3 +3,9 @@ let course2 = { code: 'ACIT 1630',  name: 'Database Systems'}
 let course3 = { code: 'ACIT 1515',  name: 'Scripting for IT'}
 
 const courseList = [course1, course2, course3]
+
+let user;
+do{
+    user= prompt('Enter a course code: ')
+}while(isNaN(user) || user.length !== 4);
+
