9d830bed4fbcc29a46015059f10af916beb303c8
Jolly <jmurphy106@my.bcit.ca>
2021-04-01 03:34:59 -0700

    fixed mistakes from feedback for lab 6. Added a java folder and script file, with an array containting course objects. Linked this to courses.html

---
+++ java/script.js
@@ -0,0 +1,5 @@
+let course1 = { code: 'ACIT 1620',  name: 'Web Fundamental Technologies'}
+let course2 = { code: 'ACIT 1630',  name: 'Database Systems'}
+let course3 = { code: 'ACIT 1515',  name: 'Scripting for IT'}
+
+const courseList = [course1, course2, course3]
